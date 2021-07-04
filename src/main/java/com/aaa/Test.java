00511c2fbc9c12153c5a60e292fb7c5564cb5b6b
zhaoyichao <421905228@qq.com>
2021-07-04 20:12:05 +0800

    first hot-fix commit

--- src/main/java/com/aaa/Test.java
+++ src/main/java/com/aaa/Test.java
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("hellow git!");
         System.out.println("hellow git!");
         System.out.println("hellow git!");
+        System.out.println("hellow git!");
     }
 }
